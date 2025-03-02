c47b3310ab54d63fb31a51ef6f1ce8cd2f88cb17
Saeed <alktbihd@gmail.com>
2025-03-02 22:04:58 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
+const cors = require('cors')
 const port = 8080
 
-var corsOptions = {
+
+const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
 
-  app.get('/', cors(corsOptions), function (req, res, next) {
-    res.send('Hello World!')
-  })
+
+app.get('/api/wake', cors(corsOptions), (req, res) => {
+    res.json({ status: 'Server is awake' })
+})
+
+app.get('/', cors(corsOptions), (req, res) => {
+    res.send('Dice Roller API is running!')
+})
+
+app.get('/api/roll', cors(corsOptions), (req, res) => {
+    const result = Math.floor(Math.random() * 6) + 1
+        res.json({ result })
+})
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Dice Roller API running on port ${port}`)
 })
